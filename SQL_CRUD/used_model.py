670fc814cdf2542fa9de1b5d35e140a1194c254f
S2eohyun <109630259+arim5123@users.noreply.github.com>
2022-08-23 16:21:23 +0900

    Update used_model.py

--- SQL_CRUD/used_model.py
+++ SQL_CRUD/used_model.py
@@ -3,6 +3,7 @@ $val = $model->where('num', $num)->first();
 $file_name = $val['file_name'];
 
 
+
 //select_where
 $model->where('num',$num)->select('project')->find();
 $project = $query[0]['project'];
@@ -11,6 +12,18 @@ $project = $query[0]['project'];
 
 //selectAll
 $project = $model->find($num);
+
+
+
+//selectMax
+$get_max = $model->selectMax('post_id')->find();
+$post_id = (int)$get_max[0]['post_id'];
+
+
+
+//insert _ 특정 data insert
+$data = ['post_id' => $post_id, 'title' => $title];
+$model->insert($data);
 
 
 
@@ -23,9 +36,7 @@ $data = [
   'part_detail'   => $n_part_detail,
   'charge'        => $charge
 ];
-
 $result = $model->update($num, $data);  //OR => $model->set($data)->where('num',$num)->update();
-
 if (!empty($result)) {
   $this->response->redirect("/---");
 }
